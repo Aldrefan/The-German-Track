9613a91be5e4241782643d35d3c876c829aaa77f
benjilap <benjilap2@yahoo.fr>
2019-10-28 13:35:57 +0100

    [ADD] LeonPanel

---
+++ Unity PFA Project/Assets/DevFiles/Benji/Scripts/DialogData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogData : MonoBehaviour
+{
+
+    public string characName;
+
+    Sprite characSprite;
+
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Init()
+    {
+        if (characName != null)
+        {
+            characSprite = Resources.Load("Resources/Sprites" + characName) as Sprite;
+        }
+        if(characSprite != null)
+        {
+            this.transform.Find("CharacterPict").GetChild(0).GetComponent<Image>().sprite = characSprite;
+            this.transform.Find("CharacterName").GetChild(0).GetComponent<Text>().text = characName;
+        }
+    }
+}
+
+public class SentencePatern
+{
+    public int index;
+    public string speaker;
+    public string fullSentence;
+    public int indexOutput;
+
+    public SentencePatern(int newIndex, string newSpeaker, string newFullSentence, int newIndexOutput)
+    {
+        index = newIndex;
+        speaker = newSpeaker;
+        fullSentence = newFullSentence;
+        indexOutput = newIndexOutput;
+    }
+}
