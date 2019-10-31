537a2282ef20f2a8cfb22ad79d7add0f95569077
benjilap <benjilap2@yahoo.fr>
2019-10-31 12:28:08 +0100

    [UP] Prep Interract

---
+++ Unity PFA Project/Assets/DevFiles/Benji/Scripts/LeonDialogueManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeonDialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences = new Queue<string>();
+
+    [HideInInspector]
+    public string npcName;
+    string playerName = "Léon";
+
+    Sprite npcSprite;
+    Sprite leonSprite;
+
+    GameObject DialogWidget;
+
+
+
+    void Start()
+    {
+        DialogWidget = this.transform.GetChild(0).gameObject;
+        InitPlayerSide();
+        Enable(DialogWidget.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitDialogWidget()
+    {
+        Enable(DialogWidget.transform,true);
+        if (npcName != null)
+        {
+            npcSprite = Resources.Load("Resources/Sprites" + npcName) as Sprite;
+        }
+        if(npcSprite != null)
+        {
+            this.transform.Find("CharacterPict").GetChild(0).GetComponent<Image>().sprite = npcSprite;
+            this.transform.Find("CharacterName").GetChild(0).GetComponent<Text>().text = npcName;
+        }
+    }
+
+    void Enable(Transform transform,bool state)
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name != "ChoicePanel")
+            {
+                child.gameObject.SetActive(state);
+
+                Enable(child, state);
+            }
+        }
+
+    }
+
+    void InitPlayerSide()
+    {
+        if (playerName != null)
+        {
+            leonSprite = Resources.Load("Resources/Sprites" + playerName) as Sprite;
+        }
+        if (leonSprite != null)
+        {
+            this.transform.Find("CharacLeft").GetChild(0).Find("CharacterPict").GetChild(0).GetComponent<Image>().sprite = leonSprite;
+        }
+    }
+
+    
+
+
+}
+
---
+++ Unity PFA Project/Assets/DevFiles/Benji/Scripts/Test/PersoTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoTest : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 1;
+
+    bool inDialog;
+
+    GameObject DialogCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FindObjectOfType<LeonDialogueManager>() != null)
+        {
+            DialogCanvas = FindObjectOfType<LeonDialogueManager>().gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamFollow();
+        if (!inDialog)
+        {
+            Movement();
+
+        }
+    }
+
+    void Movement()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed, this.GetComponent<Rigidbody2D>().velocity.y);
+        }else if (this.GetComponent<Rigidbody2D>().velocity.magnitude!=0)
+        {
+            this.GetComponent<Rigidbody2D>().velocity -= new Vector2(this.GetComponent<Rigidbody2D>().velocity.x, 0);
+        }
+    }
+
+    void CamFollow()
+    {
+        Vector3 targetPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 10);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, targetPos, 0.5f);
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PNJinteractable"))
+        {
+
+            Debug.Log(collision.name);
+            if (collision.GetComponent<DialogueTrigger>() != null)
+            {
+                Debug.Log("2");
+
+                if (Input.GetButton("Interaction"))
+                {
+                    Debug.Log("3");
+                    inDialog = true;
+                    this.DialogCanvas.GetComponent<LeonDialogueManager>().npcName = collision.GetComponent<DialogueTrigger>().npcName;
+                    this.DialogCanvas.GetComponent<LeonDialogueManager>().InitDialogWidget();
+                }
+            }
+        }
+    }
+}
