05d3f6664bddb238f59a1dedc116da1ed197e079
benjilap <benjilap2@yahoo.fr>
2019-11-27 12:40:09 +0100

    up

---
+++ Unity PFA Project/Assets/DevFiles/Benji/Scripts/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue : MonoBehaviour
+{
+    public string speaker;
+    public string sentence;
+    public int dialogIndex;
+    public int moodIndex;
+
+}
---
+++ Unity PFA Project/Assets/DevFiles/Benji/Scripts/DialogueTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public int moodIndex = 2;
+    public string npcName;
+    public List<Dialogue> dialogue = new List<Dialogue>();
+}
