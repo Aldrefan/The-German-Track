895884f4471f8df1f9ffeab3f013a48cf9b4c833
benjilap <benjilap2@yahoo.fr>
2019-11-29 13:26:39 +0100

    up notif

---
+++ Unity PFA Project/Assets/Script/Systemes/SaveSystem/GameData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    
+    //public int level = 0;
+    public GameObject actualRoom;
+    //public List<int> stickersIndexOnBoard;
+    //public List<Vector3> stickersPositionOnBoard;
+    //public List<int> memoryStickers;
+    //public List<string> meetingList;
+    //public string language = "french";
+    public List<string> eventList;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
+public class CharacterPosition
+{
+    public GameObject character;
+    public Vector3 position;
+
+}
