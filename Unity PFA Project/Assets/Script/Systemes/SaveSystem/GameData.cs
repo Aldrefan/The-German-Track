dfb1ad00223f7a646774e82b8c9992dd5ad139c6
benjilap <benjilap2@yahoo.fr>
2019-11-29 15:07:51 +0100

    Up partial savesystem

--- Unity PFA Project/Assets/Script/Systemes/SaveSystem/GameData.cs
+++ Unity PFA Project/Assets/Script/Systemes/SaveSystem/GameData.cs
@@ -2,6 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class SaveFile : MonoBehaviour
+{
+    void Save()
+    {
+        GameData saveObject = new GameData();
+        string json = JsonUtility.ToJson(saveObject);
+
+        GameSaveSystem.Save(json);
+    }
+
+    void Load()
+    {
+        string saveString = GameSaveSystem.Load();
+        if (saveString != null)
+        {
+            GameData saveObject = JsonUtility.FromJson<GameData>(saveString);
+            ReturnGameData(saveObject);
+        }
+    }
+
+    public void ReturnGameData(GameData gameSave)
+    {
+        if (!gameSave.actualRoom.activeInHierarchy)
+        {
+            FindObjectOfType<CameraFollow>().actualRoom.SetActive(false);
+            FindObjectOfType<CameraFollow>().actualRoom = gameSave.actualRoom;
+            FindObjectOfType<CameraFollow>().actualRoom.SetActive(true);
+        }
+
+        if (FindObjectOfType<ActiveCharacterScript>().playableCharactersList.Count != gameSave.playableCharacters.Count)
+        {
+            FindObjectOfType<ActiveCharacterScript>().playableCharactersList.Clear();
+            for (int i = 0; i < gameSave.playableCharacters.Count; i++)
+            {
+                GameObject[] playableCharacterInScene = GameObject.FindGameObjectsWithTag("Player");
+                if (playableCharacterInScene != null)
+                {
+                    foreach(GameObject character in playableCharacterInScene)
+                    {
+                        if(gameSave.playableCharacters[i].character.name == character.name)
+                        {
+                            character.transform.position = gameSave.playableCharacters[i].position;
+                        }
+                    }
+                    
+                }
+                FindObjectOfType<ActiveCharacterScript>().playableCharactersList.Add(new ActiveCharacterScript.PlayableCharacter(gameSave.playableCharacters[i].character, true));
+            }
+        }
+
+
+    }
+}
+
+
 public class GameData : MonoBehaviour
 {
     
@@ -12,24 +67,32 @@ public class GameData : MonoBehaviour
     //public List<int> memoryStickers;
     //public List<string> meetingList;
     //public string language = "french";
-    public List<string> eventList;
+    //public List<string> eventList;
+    public List<CharacterPosition> playableCharacters; 
 
-
-    void Start()
+    public GameData()
     {
-        
+        actualRoom = FindObjectOfType<CameraFollow>().actualRoom;
+
+        for (int i = 0;i< FindObjectOfType<ActiveCharacterScript>().playableCharactersList.Count; i++){
+            playableCharacters.Add(new CharacterPosition(FindObjectOfType<ActiveCharacterScript>().playableCharactersList[i].character));
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
 }
+
 public class CharacterPosition
 {
     public GameObject character;
     public Vector3 position;
+
+    public CharacterPosition(GameObject newCharacter)
+    {
+        character = newCharacter;
+        position = newCharacter.transform.position;
+    }
 
 }
