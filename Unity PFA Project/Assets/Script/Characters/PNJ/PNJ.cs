34ecde9a24b59510b836eab3ed533566438fcaa9
Dylan <brousse.dylan@gmail.com>
2020-02-13 10:04:54 +0100

    FIX] Dialogs

--- Unity PFA Project/Assets/Script/Characters/PNJ/PNJ.cs
+++ Unity PFA Project/Assets/Script/Characters/PNJ/PNJ.cs
@@ -293,9 +293,12 @@ public class PNJ : MonoBehaviour
         quoteFinished = false;
         //Debug.Log(allDialogs.listOfDialogs[dialogIndex].dialog[dialogLine].quote);
         actualQuote = LanguageManager.Instance.GetDialog(allDialogs.listOfDialogs[dialogIndex].dialog[dialogLine].quote);
+        string openBalise = "<material=2>";
+        string closeBalise = "</material>";
+        actualQuote = openBalise + actualQuote + closeBalise;
 
-        panel.transform.GetChild(1).GetComponent<Text>().text = actualQuote;
-        actualQuote = panel.transform.GetChild(1).GetComponent<Text>().text;
+        //panel.transform.GetChild(1).GetComponent<Text>().text = actualQuote;
+        //actualQuote = panel.transform.GetChild(1).GetComponent<Text>().text;
         //int length = 0;
         //if(actualQuote.Contains("<b>"))
         //{
@@ -315,7 +318,7 @@ public class PNJ : MonoBehaviour
                 i += 3;
             }*/
             currentLine = actualQuote.Substring(0,i);
-            panel.transform.GetChild(1).GetComponent<Text>().text = currentLine;
+            panel.transform.GetChild(1).GetComponent<TextMesh>().text = currentLine;
             yield return new WaitForSeconds(dialogDelay);
         }
         quoteFinished = true;
