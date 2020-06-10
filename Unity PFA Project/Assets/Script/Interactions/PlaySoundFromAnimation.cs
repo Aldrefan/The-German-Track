685df047b445f3d10a9a0e6c9dfbd64d41f33abe
Dylan <brousse.dylan@gmail.com>
2020-06-10 09:02:14 +0200

    [UP] ptits trucs

---
+++ Unity PFA Project/Assets/Script/Interactions/PlaySoundFromAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundFromAnimation : MonoBehaviour
+{
+    public void Animation_PlaySound(AudioClip clip)
+    {
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+        source.PlayOneShot(clip);
+        StartCoroutine(Timer(clip.length));
+    }
+
+    IEnumerator Timer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(GetComponent<AudioSource>());
+    }
+}
